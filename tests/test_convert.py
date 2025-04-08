904d66d37f9d9388b652ba94862fd264a6b9bd75
Edward Linscott <edwardlinscott@gmail.com>
2025-04-08 12:23:16 +0200

    Fix incorrect type for ElementTree

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,26 +1,27 @@
 """Testing the `wannier90_input.convert` module."""
 
+from pathlib import Path
+from xml.etree.ElementTree import ElementTree
+
 import pytest
 from defusedxml.ElementTree import parse
-from xml.etree.ElementTree import Element
-from pathlib import Path
 
 from wannier90_input.convert import InvalidXMLStructureError, convert_xml_tree_to_model
 
 
 @pytest.fixture
-def example_xml(data_directory: Path) -> Element:
+def example_xml(data_directory: Path) -> ElementTree:
     """Return the filepath of an example XML file,"""
     xml = parse(data_directory / "valid.xml")
-    assert isinstance(xml, Element)
+    assert isinstance(xml, ElementTree)
     return xml
 
-def test_convert_valid_xml(example_xml: Element) -> None:
+def test_convert_valid_xml(example_xml: ElementTree) -> None:
     """Test converting valid XML."""
     convert_xml_tree_to_model(example_xml)
 
 @pytest.mark.parametrize("missing_field", ["name", "type", "description"])
-def test_convert_xml_missing_field(missing_field: str, example_xml: Element) -> None:
+def test_convert_xml_missing_field(missing_field: str, example_xml: ElementTree) -> None:
     """Test invalid XML that is missing a field."""
     # Remove the field "missing_field" from the valid xml
     field_removed = False
